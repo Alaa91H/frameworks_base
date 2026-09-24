c40d8a235aaf848c85804050ce0b3f49cd369008
Alaa <alahus2591@gmail.com>
2026-09-24 23:31:09 +0200

    SystemUI: remove dead smartspace decompiler leftovers

--- packages/SystemUI/src/com/google/android/systemui/smartspace/BcSmartspaceView.java
+++ packages/SystemUI/src/com/google/android/systemui/smartspace/BcSmartspaceView.java
@@ -590,7 +590,6 @@ public class BcSmartspaceView extends FrameLayout
         setPivotY(desiredHeight / 2.0f);
     }
 
-    // ???
     public final void onSmartspaceTargetsUpdated(
             List<? extends Parcelable> targets, Runnable runnable) {
         List<SmartspaceTarget> smartspaceTargets =
@@ -910,14 +909,6 @@ public class BcSmartspaceView extends FrameLayout
         }
     }
 
-    // DOES NOT EXIST ???
-    public final void setSelectedPage(int i) {
-        mViewPager2.post(
-                () -> {
-                    mViewPager2.setCurrentItem(i, false);
-                });
-        setSelectedDot(0.0f, i);
-    }
 
     @Override
     public final void setSplitShadeEnabled(boolean enabled) {
